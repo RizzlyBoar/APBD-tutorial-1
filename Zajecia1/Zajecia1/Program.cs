456d9f3d4fbd1a72f93bb55921ad6310262eb1c8
s27538 <s27538@pjwstk.edu.pl>
2024-03-11 13:08:23 +0100

    Pierwsza wersja projektu

---
+++ Zajecia1/Zajecia1/Program.cs
@@ -0,0 +1,25 @@
+﻿public class Program
+{
+    //metody z Duzej litery!!
+    
+    public static void Main(string[] args)
+    {
+    //primitive
+         int g = 10;
+         int g2 = g;//kopiujemy wartosc
+         
+    //complex
+    string s = "ala";
+    string s2 = s;
+    int[] arr = { 2, 4, 5, 7 };
+    int[] arr2 = arr;
+    arr2[0] = 222;
+    int? age = null;
+    //Nullable<int> age = null;
+    //Integer age.. <- java
+
+    Console.WriteLine("Hello, World!");
+    }
+
+    public static void DoSth(){}
+}
